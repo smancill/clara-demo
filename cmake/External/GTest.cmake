d071d5e5dec48a2419d63c2183453742d39c520d
Sebastián Mancilla <smancill@jlab.org>
2019-02-08 18:47:47 -0300

    cmake: general updates
    - Bump minimum version.
    
    - Update build type configuration by using a cache variable.
      And add a list of available types to choose from.
    
    - Rename variable to hint location of OpenCV
    
      Also enable policy CMP0074 (introduced in CMake 3.12) to allow naming
      the variable "<package>_ROOT", which with the new policy is used by
      find_package as first search prefix. The variable still needs to be
      passed as hints because the minimum supported version of CMake is 3.5.
    
    - Fix Clara package name.
    
    - Update macOS install rpath to use @loader_path.
    
    - Update GTest/GMock external project to latest version, and use
      proper target names.
    
    - Do not force building shared libraries for "data" and "legacy"
      subprojects. The services do need to be shared libraries though,
      because they are dynamically loaded.
    
    - Miscellaneous style fixes.

--- cmake/External/GTest.cmake
+++ cmake/External/GTest.cmake
@@ -2,44 +2,44 @@ INCLUDE(ExternalProject)
 SET_DIRECTORY_PROPERTIES(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/ThirdParty)
 
 ExternalProject_Add(
-  googlemock
-  URL https://github.com/google/googletest/archive/release-1.8.0.zip
-  URL_HASH SHA1=667f873ab7a4d246062565fad32fb6d8e203ee73
-  TIMEOUT 30
+  googletest
+  URL https://github.com/google/googletest/archive/release-1.8.1.zip
+  URL_HASH SHA256=927827c183d01734cc5cfef85e0ff3f5a92ffe6188e0d18e909c5efebf28a0c7
+  TIMEOUT 60
   INSTALL_COMMAND ""
   LOG_DOWNLOAD ON
   LOG_CONFIGURE ON
   LOG_BUILD ON
 )
 
-find_package(Threads)
+find_package(Threads QUIET)
 
-ExternalProject_Get_Property(googlemock SOURCE_DIR)
-ExternalProject_Get_Property(googlemock BINARY_DIR)
+ExternalProject_Get_Property(googletest SOURCE_DIR)
+ExternalProject_Get_Property(googletest BINARY_DIR)
 
 # avoid error with set_target_properties
 file(MAKE_DIRECTORY "${SOURCE_DIR}/googlemock/include")
 file(MAKE_DIRECTORY "${SOURCE_DIR}/googletest/include")
 
-add_library(gmock IMPORTED STATIC IMPORTED)
-add_library(gtest IMPORTED STATIC IMPORTED)
+add_library(GTest::GTest IMPORTED STATIC IMPORTED)
+add_library(GTest::GMock IMPORTED STATIC IMPORTED)
 
-set_target_properties(gtest PROPERTIES
+set_target_properties(GTest::GTest PROPERTIES
   IMPORTED_LOCATION
     "${BINARY_DIR}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}"
   INTERFACE_INCLUDE_DIRECTORIES
     "${SOURCE_DIR}/googletest/include"
   IMPORTED_LINK_INTERFACE_LIBRARIES
-    "${CMAKE_THREAD_LIBS_INIT}"
+    Threads::Threads
 )
 
-set_target_properties(gmock PROPERTIES
+set_target_properties(GTest::GMock PROPERTIES
   IMPORTED_LOCATION
     "${BINARY_DIR}/googlemock/${CMAKE_FIND_LIBRARY_PREFIXES}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}"
   INTERFACE_INCLUDE_DIRECTORIES
     "${SOURCE_DIR}/googlemock/include"
   IMPORTED_LINK_INTERFACE_LIBRARIES
-    gtest
+    GTest::GTest
 )
 
-add_dependencies(gmock googlemock)
+add_dependencies(GTest::GMock googletest)
